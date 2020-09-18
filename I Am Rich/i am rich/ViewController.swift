25d8ea2aff03cce31dd72efb2a0598e240b52d1d
Natallyson Saraiva <natallyson12@gmail.com>
2020-09-18 10:33:52 -0400

    Add files via upload
    Primeiro commit.

---
+++ I Am Rich/i am rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  i am rich
+//
+//  Created by Natallyson Saraiva on 14/09/20.
+//  Copyright © 2020 Natallyson Saraiva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
