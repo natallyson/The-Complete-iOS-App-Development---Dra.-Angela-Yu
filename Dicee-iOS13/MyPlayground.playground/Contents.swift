8c2ba3beaa5f4464c8a8b49b5fac267c2390c7f3
Natallyson Saraiva <natallyson12@gmail.com>
2020-09-21 13:48:57 -0400

    Add files via upload
    App DICEE

---
+++ Dicee-iOS13/MyPlayground.playground/Contents.swift
@@ -0,0 +1,19 @@
+print("hello world")
+
+print("hello 2+5 world")
+
+print("hello \(2+5) world")
+
+print("The Result of 5 + 3 = \(5+3)")
+
+print(Int.random(in: 1...10))
+
+
+
+
+
+var numbers = [45, 73, 195, 53]
+
+var computedNumbers = [numbers[0] * numbers[1]]
+
+print("\(computedNumbers)")
