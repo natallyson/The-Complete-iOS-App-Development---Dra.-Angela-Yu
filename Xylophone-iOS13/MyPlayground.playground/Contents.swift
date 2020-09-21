808a5f972cb5e563891a8e76297e5c88b5ba71b2
Natallyson Saraiva <natallyson12@gmail.com>
2020-09-21 19:33:47 -0400

    Completed App (project)

---
+++ Xylophone-iOS13/MyPlayground.playground/Contents.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+//let a = Int(readLine()!)! //First input
+//let b = Int(readLine()!)! //Second input
+
+func add(a: Int, b: Int){
+    let resultado = a + b
+    print("\(a) + \(b) = \(resultado)")
+}
+
+func subtract(a: Int, b: Int){
+    let resultado = a - b
+    print("\(a) - \(b) = \(resultado)")
+}
+
+func multiply(a: Int, b: Int){
+    let resultado = a * b
+    print("\(a) * \(b) = \(resultado)")
+}
+
+func divide(a: Int, b: Int){
+    let resultado = a / b
+    print("\(a) / \(b) = \(resultado)")
+}
+add(a: 2, b: 1)
+subtract(a: 2, b: 1)
+multiply(a: 2, b: 1)
+divide(a: 2, b: 1)
