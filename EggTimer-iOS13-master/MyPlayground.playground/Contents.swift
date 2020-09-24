09569b746a9080469736c5403fd4e5ff257b997e
Natallyson Saraiva <natallyson12@gmail.com>
2020-09-24 15:19:19 -0400

    App completed.

---
+++ EggTimer-iOS13-master/MyPlayground.playground/Contents.swift
@@ -0,0 +1,46 @@
+//func loveCalculator(){
+//    let loveScore = Int.random(in: 0...100)
+//
+//    if loveScore >= 80 {
+//        print("You love each other like Kanye loves Kanye.")
+//    } else if loveScore > 40 {
+//        print("You go together likeCoke and Mentos")
+//    } else{
+//        print("You'll be forever alone.")
+//    }
+//}
+//loveCalculator()
+
+
+
+//func loveCalculator(){
+//    let loveScore = Int.random(in: 0...100)
+//
+//    if loveScore > 80 {
+//        print("You love each other like Kanye loves Kanye.")
+//    } else if loveScore > 40 && loveScore <= 80 {
+//        print("You go together like Coke and Mentos")
+//    } else{
+//        print("You'll be forever alone.")
+//    }
+//}
+//loveCalculator()
+
+
+
+//func loveCalculator(){
+//    let loveScore = Int.random(in: 0...100)
+//
+//    switch loveScore {
+//    case 81...100:
+//        print("You love each other like Kanye loves Kanye.")
+//    case 41...80:
+//        print("You go together like Coke and Mentos")
+//    case ...41:
+//        print("You'll be forever alone.")
+//    default:
+//        print("Error lovescore is out of range.")
+//    }
+//}
+//loveCalculator()
+
