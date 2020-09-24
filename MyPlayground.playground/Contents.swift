73769ecd09f8299ea4739272c3d5c8491a4ff1a3
Natallyson <natallyson12@mail.com>
2020-09-24 15:33:58 -0400

    Upload Playground test.

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,13 @@
+////Don't change this
+var aNumber =  Int(readLine()!)!
+
+func dayOfTheWeek(day: Int) {
+  
+  //Write your code inside this function.
+  
+  
+  
+}
+
+//Don't change this
+dayOfTheWeek(day: aNumber)
