cff63270f613500091c3271bf727372f4f9f23e5
Natallyson <natallyson12@mail.com>
2020-10-13 09:02:20 -0400

    completing the project.

--- Módulo 11/BMI-Calculator-iOS13-master/BMI Calculator/Controllers/ResultViewController.swift
+++ Módulo 11/BMI-Calculator-iOS13-master/BMI Calculator/Controllers/ResultViewController.swift
@@ -2,8 +2,8 @@
 //  ResultViewController.swift
 //  BMI Calculator
 //
-//  Created by Natallyson Saraiva on 09/10/20.
-//  Copyright © 2020 Angela Yu. All rights reserved.
+//  Created by Angela Yu on 28/08/2019.
+//  Copyright © 2019 Angela Yu. All rights reserved.
 //
 
 import UIKit
@@ -11,20 +11,23 @@ import UIKit
 class ResultViewController: UIViewController {
     
     var bmiValue: String?
-    
+    var advice: String?
+    var color: UIColor?
+
     @IBOutlet weak var bmiLabel: UILabel!
     @IBOutlet weak var adviceLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         bmiLabel.text = bmiValue
+        adviceLabel.text = advice
+        view.backgroundColor = color
     }
     
     @IBAction func recalculatePressed(_ sender: UIButton) {
-        self.dismiss(animated: true, completion: nil)
+        dismiss(animated: true, completion: nil)
     }
-    
     
 
 }
