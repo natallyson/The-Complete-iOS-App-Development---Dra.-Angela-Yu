6c9213c5e652b5a61944db30bafda8b24feb5beb
Natallyson <natallyson12@mail.com>
2020-10-14 03:22:29 -0400

    Written as screen transitions. Sending the result to a result screen. Created as variables in the ResultViewController. sending the data to as TextField. Showing the result on the screen.

--- Módulo 12/Tipsy-iOS13/Tipsy/Controllers/CalculatorViewController.swift
+++ Módulo 12/Tipsy-iOS13/Tipsy/Controllers/CalculatorViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class CalculatorViewController: UIViewController {
-
+    
     @IBOutlet weak var billTextField: UITextField!
     @IBOutlet weak var zeroPctButton: UIButton!
     @IBOutlet weak var tenPctButton: UIButton!
@@ -19,17 +19,18 @@ class CalculatorViewController: UIViewController {
     var tip = 0.10
     var numberOfPeople = 2
     var billTotal = 0.0
+    var finalResult = "0.0"
     
     @IBAction func tipChanged(_ sender: UIButton) {
         
         // Dispensa o teclado quando o usuário escolhe um dos valores de ponta.
         billTextField.endEditing(true)
-       
+        
         //Desmarque todos os botões de dica via IBOutlets.
         zeroPctButton.isSelected = false
         tenPctButton.isSelected = false
         twentyPctButton.isSelected = false
-       
+        
         //Faça com que o botão que acionou a IBAction seja selecionado.
         sender.isSelected = true
         
@@ -44,33 +45,49 @@ class CalculatorViewController: UIViewController {
         
         //Divida a porcentagem expressa de 100 em um decimal, por exemplo 10 torna-se 0,1
         tip = buttonTitleAsANumber / 100
-
+        
     }
     
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
-       
+        
         //Obtenha o valor do passo usando sender.value, arredonde-o para um número inteiro e defina-o como o String em splitNumberLabel
         splitNumberLabel.text = String(format: "%.0f", sender.value)
-    
+        
         // Defina a propriedade numberOfPeople como o valor do stepper como um número inteiro.
         numberOfPeople = Int(sender.value)
     }
     
     @IBAction func calculatePressed(_ sender: UIButton) {
+        
         // Pega o texto que o usuário digitou no billTextField
         let bill = billTextField.text!
         
         // Se o texto não for uma String vazia ""
         if bill != "" {
-        // Transforme a nota de uma String, por exemplo "123,50" para uma string real com casas decimais.
+            // Transforme a nota de uma String, por exemplo "123,50" para uma string real com casas decimais.
             billTotal = Double(bill)!
             
             // Multiplique a conta pela porcentagem da gorjeta e divida pelo número de pessoas para dividir a conta.
             let result = billTotal * (1 + tip) / Double(numberOfPeople)
             
             // Arredonde o resultado para 2 casas decimais e transforme-o em uma String.
-            let resultTo2DecimalPlaces = String(format: "%.2f", result)
-            print(resultTo2DecimalPlaces)
+            finalResult = String(format: "%.2f", result)
+        }
+        // Em Main.storyboard há uma transição entre CalculatorVC e ResultsVC com o identificador "goToResults".
+        // Esta linha aciona o segue para acontecer.
+        self.performSegue(withIdentifier: "goToResults", sender: self)
+    }
+        // Este método é acionado antes do início da transição.
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?){
+            // Se a segue acionada atualmente é a segue "goToResults".
+            if segue.identifier == "goToResults" {
+                // Obtenha a instância do VC de destino e digite cast para um ResultViewController.
+                let destinationVC = segue.destination as! ResultsViewController
+                // Defina as propriedades do ResultsViewController de destino.
+                destinationVC.result = finalResult
+                destinationVC.tip = Int(tip * 100)
+                destinationVC.split = numberOfPeople
+            }
         }
     }
-}
+
