60a64f1fcf7b0a9a97e6eeb7d1f2efe194186b40
Natallyson <natallyson12@mail.com>
2020-11-24 17:59:47 -0400

    implementing an animation cocoapods for home screen

---
+++ Módulo 15/MyPlayground.playground/Contents.swift
@@ -0,0 +1,25 @@
+class Assignment {
+    
+    func fibonacci(n: Int) {
+        
+        var n1 = 0
+        var n2 = 1
+        
+        if n == 0 {
+            print("Invalid")
+        } else if n == 1 {
+            print(n1)
+        } else if n == 2 {
+            print(n1, n2)
+        } else {
+            var array = [n1, n2]
+            for _ in 2..<n {
+                let n3 = n1 + n2
+                n1 = n2
+                n2 = n3
+                array.append(n3)
+            }
+            print(array)
+        }
+    }
+}
